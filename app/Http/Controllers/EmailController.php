2e8d4d93460fb3b3e24c9c65551ee359f1859cce
Alvin <alvinrenard09@gmail.com>
2021-10-17 23:44:50 +0700

    Email with proper links

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -1,18 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
- 
 use Illuminate\Http\Request;
- 
- 
+use App\Models\Employee;
 use App\Mail\EmployeeEmail;
 use Illuminate\Support\Facades\Mail;
  
 class EmailController extends Controller
 {
-	public function index(){
- 
-		Mail::to("fooslammers@gmail.com")->send(new EmployeeEmail());
+	public function index($id){
+
+		Mail::to("fooslammers@gmail.com")->send(new EmployeeEmail($id));
  
 		return "Email telah dikirim";
  
